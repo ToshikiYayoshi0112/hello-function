0dc24500f951d8937731f55e7ff97516b4fd161c
Yayoshi <sagauyayoshi@gmail.com>
2019-05-03 19:42:26 +0900

    関数定義の方法を学んだ

---
+++ app.py
@@ -0,0 +1,40 @@
+# def hello():
+#     print("Hello World")
+#
+#
+# hello()
+#
+#
+# def say_hello(name):
+#     print(f"Hi {name}")
+#
+#
+# say_hello("Bob")
+
+
+def double(number):
+    return 2 * number
+
+
+result_1 = double(3)
+
+print(result_1)
+
+
+# 1分課題
+def str_combine(str1, str2):
+    return f"{str1} {str2}"
+
+
+result = str_combine("Kazuma", "Takahashi")
+
+print(result)
+
+# str_combine("Kazuma", "Takahashi")
+
+
+# def str_combine():
+#     return "Kazuma", "Takahashi"
+
+
+# print(str_combine())
