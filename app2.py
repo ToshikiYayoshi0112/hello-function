95d48aa32cd0bb90cbf85824943153abf8014a56
Yayoshi <sagauyayoshi@gmail.com>
2019-05-03 20:59:59 +0900

    importの仕方を学んだ option + Enter -> import thisでPyCharmで勝手につくってくれる comand + Shift + a -> rename でimportの別ファイルのやつも変更してくれる

---
+++ app2.py
@@ -0,0 +1,4 @@
+# 割り勘したい 7000円を3人
+from warikan2 import caluclate_warikan
+
+print(caluclate_warikan(amount=7000, number_of_people=6))
--- warikan2.py
+++ warikan2.py
@@ -4,12 +4,12 @@
 
 """
 
-def warikan(amount, number_of_people):
+
+def caluclate_warikan(amount, number_of_people):
     quotinet = amount // number_of_people
     remainder = amount % number_of_people
     return f'1人あたり:{quotinet}円, 端数: {remainder}円'
 
-
-print(warikan(amount=1500, number_of_people=3))
-print(warikan(amount=2000, number_of_people=3))
-print(warikan(amount=3647, number_of_people=4))
+# print(warikan(amount=1500, number_of_people=3))
+# print(warikan(amount=2000, number_of_people=3))
+# print(warikan(amount=3647, number_of_people=4))
